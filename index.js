efbcb278e778eeca6063d775e304b05ee95f0a60
Meka Seba <mekasebameka@gmail.com>
2022-07-11 19:27:22 -0700

    fuck youuuuuuuuuuuuuuuu

---
+++ index.js
@@ -0,0 +1,8 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let name = "Meka"
+let greeting = "Welcome back "
+
+welcomeEl.innerText = greeting + name 
+
+welcomeEl.innerText = welcomeEl.innerText + "👋"
